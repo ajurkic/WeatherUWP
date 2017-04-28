feb4748dc662bf3739950bcf6d83726533f0ad1b
ajurkic <poohpack@mailinator.com>
2017-04-28 11:11:52 +0200

    Changed weather icons, added AutoSuggestBox, Chosing city made possible

--- WeatherUWP/MainPage.xaml.cs
+++ WeatherUWP/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -27,18 +28,50 @@ namespace WeatherUWP
             this.InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        /*private async void AutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
         {
             RootObject myWeather = await OpenWeatherMapProxy.GetWeather("Split");
-            if(myWeather.city != null)
+            if (myWeather.city != null)
             {
-                ResultTextBox.Text = myWeather.city.name +" - "+ myWeather.list[0].weather[0].main + 
-                                        "\nTemp.: " + myWeather.list[0].main.temp_max + " °C\n" +
-                                        "Wind speed: " +myWeather.list[0].wind.speed+ " m/s";
+                ResultTextBox.Text = myWeather.city.name + ", " + myWeather.list[0].weather[0].description +
+                                        "\nTemp.: " + myWeather.list[0].main.temp + " °C" +
+                                        "\nWind speed: " + myWeather.list[0].wind.speed + " m/s" +
+                                        "\nPressure: " + myWeather.list[0].main.pressure + " hPa" +
+                                        "\nMin. temp: " + myWeather.list[0].main.temp_min + " °C" +
+                                        "\nMax. temp: " + myWeather.list[0].main.temp_max + " °C" +
+                                        "\nCloudiness: " + myWeather.list[0].clouds.all + " %";
+
+                string icon = String.Format("http://openweathermap.org/img/w/{0}.png", myWeather.list[0].weather[0].icon);
+                ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
             }
             else
             {
                 ResultTextBox.Text = "Error: Cannot get the forecast. Check your internet connection";
+            }
+        }*/
+
+        private async void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if(args.QueryText != "")
+            {
+                RootObject myWeather = await OpenWeatherMapProxy.GetWeather(args.QueryText);
+                if (myWeather.city != null)
+                {
+                    ResultTextBox.Text = myWeather.city.name + ", " + myWeather.list[0].weather[0].description +
+                                            "\nTemp.: " + myWeather.list[0].main.temp + " °C" +
+                                            "\nWind speed: " + myWeather.list[0].wind.speed + " m/s" +
+                                            "\nPressure: " + myWeather.list[0].main.pressure + " hPa" +
+                                            "\nMin. temp: " + myWeather.list[0].main.temp_min + " °C" +
+                                            "\nMax. temp: " + myWeather.list[0].main.temp_max + " °C" +
+                                            "\nCloudiness: " + myWeather.list[0].clouds.all + " %";
+
+                    string icon = String.Format("ms-appx:///Assets/WeatherIcons/{0}.png", myWeather.list[0].weather[0].icon);
+                    ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+                }
+                else
+                {
+                    ResultTextBox.Text = "Error: Cannot get the forecast. Check your internet connection";
+                }
             }
         }
     }
